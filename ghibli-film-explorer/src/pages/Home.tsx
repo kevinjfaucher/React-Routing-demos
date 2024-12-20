8f19e4e5e8cfda944de4970e098ffa2d070c2778
kevinjfaucher <kevinjfaucher@gmail.com>
2024-12-19 20:46:00 -0500

    updated links

--- ghibli-film-explorer/src/pages/Home.tsx
+++ ghibli-film-explorer/src/pages/Home.tsx
@@ -1,38 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 interface Film {
-    id: string;
-    title: string;
-    description: string;
+  id: string;
+  title: string;
+  description: string;
+  image: string;
 }
 
 const Home: React.FC = () => {
-    const [films, setFilms] = useState<Film[]>([]);
+  const [films, setFilms] = useState<Film[]>([]);
 
-    useEffect(() => {
-        fetch('https://ghibliapi.vercel.app/films')
-            .then((response) => response.json())
-            .then((data) => setFilms(data))
-            .catch((error) => console.error('Error fetching films:', error));
-    }, []);
+  useEffect(() => {
+    fetch("https://ghibliapi.vercel.app/films")
+      .then((response) => response.json())
+      .then((data) => setFilms(data))
+      .catch((error) => console.error("Error fetching films:", error));
+  }, []);
 
-    return (
-        <div>
-            <h1>Studio Ghiblie Films</h1>
-            <ul>
-                {films.map((film) => (
-                    <li key={film.id}>
-                        <h2>{film.title}</h2>
-                        <p>{film.description.slice(0,100)}...</p>
-                        <Link to={`/film/${film.id}`}>View Details</Link>
-                    </li>
-                )
-                
-                )}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div style={{ backgroundColor: "cornflowerblue" }}>
+      <h1>Studio Ghiblie Films</h1>
+      <ul>
+        {films.map((film) => (
+          <li key={film.id} style={{ listStyle: "none" }}>
+            <h2>{film.title}</h2>
+            <p>{film.description.slice(0, 100)}...</p>
+
+            <Link to={`/film/${film.id}`}>
+              <img
+                src={film.image}
+                alt={film.title}
+                style={{ width: "150px", height: "auto" }}
+              />
+            </Link>
+            <br></br>
+            <Link to={`/film/${film.id}`}>View Details</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
